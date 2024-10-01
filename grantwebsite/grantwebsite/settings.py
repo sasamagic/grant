fe364ca062f06d8ee5b369e6db1e0aa60d08d446
Aleksandra Kim <kimaleksa5@gmail.com>
2024-10-01 11:30:10 +0300

    добавила стиль главной страницы

--- grantwebsite/grantwebsite/settings.py
+++ grantwebsite/grantwebsite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main'
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- grantwebsite/main/views.py
+++ grantwebsite/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h4>Ghjkhf</h4>')
+    return render(request,'main/index.html')
 # Create your views here.
